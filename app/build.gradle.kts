9324ddca1d7bf2819644e79680fd80458508b45c
Axl47 <axelorta2153@gmail.com>
2026-02-26 13:59:46 -0400

    fix(video): optimize and limit media cache

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.theoriacodex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.2.3"
+        versionCode = 19
+        versionName = "0.2.4"
         if (versionCodeOverride != null) {
             versionCode = versionCodeOverride
         }
