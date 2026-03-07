6adb047a8f35a049dd350e5154aafe39a7e18f17
Axl47 <axelorta2153@gmail.com>
2026-03-07 04:56:52 -0400

    chore(release): Bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.theoriacodex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "0.3.0"
+        versionCode = 31
+        versionName = "0.3.1"
         if (versionCodeOverride != null) {
             versionCode = versionCodeOverride
         }
