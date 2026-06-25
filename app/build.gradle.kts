d93197c54c372df750cc4cc414afecfd8b7b0f6f
Axl47 <axelorta2153@gmail.com>
2026-06-25 04:54:54 -0400

    chore(version): bump app version to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.theoriacodex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "0.4.7"
+        versionCode = 40
+        versionName = "0.5.0"
         if (versionCodeOverride != null) {
             versionCode = versionCodeOverride
         }
